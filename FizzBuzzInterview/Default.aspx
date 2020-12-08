8c6560894017905988c58165d317e8815ee03c60
LockpickingDev <dan7gr@gmail.com>
2020-12-07 16:15:55 -0800

    Initial commit

---
+++ FizzBuzzInterview/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FizzBuzzInterview.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--Enter a string <asp:TextBox ID="userString" runat="server" DefaultButton="userString"></asp:TextBox>--%>
+            Input string array is located in backend in var: userInputString<br /><br />
+            <asp:Button ID="submitButton" runat="server" Text="Run FizzBuzz" OnClick="submitButton_Click" />
+        </div>
+
+        <div style="display:block;">
+            <b>FizzBuzz Results:</b><br />
+            <label id="FizzBuzzText" runat="server"></label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FizzBuzzInterview/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+namespace FizzBuzzInterview
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            //Given the example, it made sense to use an array of Strings
+            string[] userInputString = { "1", "2", "3", "4", "5", "A", "" };
+            FizzBuzzText.InnerHtml = FizzBuzz(userInputString);
+        }
+
+        public string FizzBuzz(string[] inputString)
+        {
+            StringBuilder returnString = new StringBuilder();
+            int currentValue;
+
+            //for (int i = 1; i <= 100; i++)
+            for (int i = 0; i <= inputString.Length - 1; i++)
+            {
+                try
+                {
+                    currentValue = Convert.ToInt32(inputString[i]);
+
+                    //Divisible by 3 and 5
+                    if (currentValue % 3 == 0 && currentValue % 5 == 0)
+                    {
+                        returnString.AppendFormat("FizzBuzz<br />");
+                    }
+
+                    //Divisible by 3
+                    if (currentValue % 3 == 0)
+                    {
+                        returnString.AppendFormat("Fizz<br />");
+                    }
+                    else
+                    {
+                        returnString.AppendFormat("Divided {0} by 3<br />", currentValue);
+                    }
+
+                    //Divisible by 5
+                    if (currentValue % 5 == 0)
+                    {
+                        returnString.AppendFormat("Buzz<br />");
+                    }
+                    else
+                    {
+                        returnString.AppendFormat("Divided {0} by 5<br />", currentValue);
+                    }
+
+                }
+                catch (Exception)
+                {
+                    returnString.AppendFormat("Invalid Item<br />");
+                }
+            }
+
+            return returnString.ToString();
+        }
+    }
+}
